c84e762b768e5a5ff859ea88872ae09d748b2e93
mamadou <lucyyyy2004@gmail.com>
2026-03-24 23:47:10 +0100

    [Feat][Add] Csfml initialization, variables renaming, functions to properly free everything.

--- src/wolf3d.c
+++ src/wolf3d.c
@@ -68,14 +68,52 @@ static void free_map(char **map)
     free(map);
 }
 
-static int ini_csfml(wolf_t *wolf)
+static int destroy_return_int(int RET_CODE, wolf_t *wolf)
 {
-    return 0;
+    if (!wolf)
+        return RET_CODE;
+    if (wolf->buttonfml)
+        buttonfml_destroy(wolf->buttonfml);
+    if (wolf->setfml)
+        setfml_destroy(wolf->setfml);
+    if (wolf->map)
+        free_map(wolf->map);
+    if (wolf->alloc)
+        c_delete(wolf->alloc, true);
+    return RET_CODE;
 }
 
-static int ini_map(wolf_t *wolf)
+static void *destroy_return_null(wolf_t *wolf)
 {
-    return 0;
+    if (!wolf)
+        return NULL;
+    if (wolf->buttonfml)
+        buttonfml_destroy(wolf->buttonfml);
+    if (wolf->setfml)
+        setfml_destroy(wolf->setfml);
+    if (wolf->map)
+        free_map(wolf->map);
+    if (wolf->alloc)
+        c_delete(wolf->alloc, true);
+    return NULL;
+}
+
+static int ini_csfml_env(wolf_t *wolf)
+{
+    setfml_t *setfml = NULL;
+    buttonfml_t *buttonfml = NULL;
+
+    if (!wolf)
+        return destroy_return_int(WOLF_FAIL, wolf);
+    setfml = setfml_ini((void *)wolf);
+    if (!setfml)
+        return destroy_return_int(WOLF_FAIL, wolf);
+    wolf->setfml = setfml;
+    buttonfml = buttonfml_ini(setfml);
+    if (!buttonfml)
+        return destroy_return_int(WOLF_FAIL, wolf);
+    wolf->buttonfml = buttonfml;
+    return WOLF_SUCC;
 }
 
 static wolf_t *ini_main(c_alloc_t *alloc)
@@ -87,13 +125,13 @@ static wolf_t *ini_main(c_alloc_t *alloc)
     wolf = c_alloc(sizeof(wolf_t), 1, alloc);
     if (!wolf) {
         c_delete(alloc, false);
-        return NULL;
+        return destroy_return_null(wolf);
     }
     wolf->alloc = alloc;
     wolf->map = create_map();
     if (!wolf->map) {
         c_delete(alloc, true);
-        return NULL;
+        return destroy_return_null(wolf);
     }
     return wolf;
 }
@@ -105,7 +143,7 @@ int main(void)
 
     if (!wolf)
         return WOLF_FAIL;
-    free_map(wolf->map);
-    c_delete(alloc, true);
-    return WOLF_SUCC;
+    if (ini_csfml_env(wolf) == WOLF_FAIL)
+        return WOLF_FAIL;
+    return destroy_return_int(WOLF_SUCC, wolf);
 }
